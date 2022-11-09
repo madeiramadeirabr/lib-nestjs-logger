33e0304810f2790c18468af96435e2e02b93e9e7
FelipeJhordan <felipejordan.alves@gmail.com>
2022-11-09 11:17:42 -0300

    feat(CAT-216): create the type/js library file

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export * from './dist/libs/nest-js-logger/nest-js-logger/src'
---
+++ index.js
@@ -0,0 +1,5 @@
+function __export(m) {
+  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+exports.__esModule = true;
+__export(require('./dist/libs/nest-js-logger/nest-js-logger/src'));
